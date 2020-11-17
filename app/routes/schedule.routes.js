4c1eab0db53a9a97fc904193471ed1c213e3ebec
yucong-guo <70935810+yucong-guo@users.noreply.github.com>
2020-11-16 19:44:23 -0600

    Update schedule.routes.js

--- app/routes/schedule.routes.js
+++ app/routes/schedule.routes.js
@@ -1,21 +1,21 @@
 module.exports = app => {
-  const Schedule = require("../controllers/schedule.controller.js");
+  const Schedule = require("../controllers/Schedule.controller.js");
 
   // Create a new schedule
-  app.post("/Schedule", schedule.create);
+  app.post("/Schedule", Schedule.create);
 
   // Retrieve all schedule
-  app.get("/Schedule", schedule.findAll);
+  app.get("/Schedule", Schedule.findAll);
 
   // Retrieve a single schedule with username
-  app.get("/Schedule/:username", schedule.findOne);
+  app.get("/Schedule/:username", Schedule.findOne);
 
   // Update a schedule with username
-  app.put("/Schedule/:username", schedule.update);
+  app.put("/Schedule/:username", Schedule.update);
 
   // Delete a schedule with username
-  app.delete("/Schedule/:username", schedule.delete);
+  app.delete("/Schedule/:username", Schedule.delete);
 
   // Create a new schedule
-  app.delete("/Schedule", users.deleteAll);
+  app.delete("/Schedule", Schedule.deleteAll);
 };
