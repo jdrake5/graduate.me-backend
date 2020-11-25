e3197748c8609eb534971f4e310ad16531ceda56
jdrake5 <jdrake8@gmail.com>
2020-11-24 20:16:10 -0600

    user login, some search

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -44,9 +44,7 @@ exports.findOne = (req, res) => {
   User.findById(req.params.username, (err, data) => {
     if (err) {
       if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found user with id ${req.params.username}.`
-        });
+        res.send(data);
       } else {
         res.status(500).send({
           message: "Error retrieving user with id " + req.params.username
