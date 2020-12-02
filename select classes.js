2c3ddd29255259c4790e560ba174e1fc95447636
yucong-guo <70935810+yucong-guo@users.noreply.github.com>
2020-12-02 15:02:18 -0600

    selected classes

--- select classes.js
+++ select classes.js
@@ -19,15 +19,5 @@ con.connect(function(err) {
 });
 
 
-#Return crn of classes that don’t conflict time wise with one another, and have minimum
-#number of requirements.
-
-con.connect(function(err,selected) {
-  if (err) throw err;
-  con.query("Select temp1.subject,temp1.number From (SELECT DISTINCT Courses.Subject as subject ,Courses.Number as number,Courses.Start_Time as number FROM Courses NATURAL JOIN selected groupby Start_Time) as temp1 " , function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
 
 
